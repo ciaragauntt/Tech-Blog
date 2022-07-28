179b7ea2f3a5a230db39013ea77e516ed3ed3dd8
cici <ci.gauntt@gmail.com>
2022-07-28 10:47:51 -0600

    started models

---
+++ Develop/controllers/index.js
@@ -0,0 +1,17 @@
+
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashRoutes = require('./dash-routes.js');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
